6825d722c4f87843dd13d8cfc2554897aa5e7393
Hugo Santana <contatodev.hugo@gmail.com>
2026-03-12 17:02:04 -0300

    fix: colocando ResponseEntity no controller

--- src/main/java/com/project/cadastroninja/Ninjas/NinjaController.java
+++ src/main/java/com/project/cadastroninja/Ninjas/NinjaController.java
@@ -1,66 +1,87 @@
 package com.project.cadastroninja.Ninjas;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @RestController
 @RequestMapping("/ninjas")
 
 public class NinjaController {
 
-   private NinjaService ninjaService;
-   public NinjaController(NinjaService ninjaService) {
+    private final NinjaRepository ninjaRepository;
+    private NinjaService ninjaService;
+   public NinjaController(NinjaService ninjaService, NinjaRepository ninjaRepository) {
        this.ninjaService = ninjaService;
+       this.ninjaRepository = ninjaRepository;
    }
 
         //add ninja CREATE
+        //o ResponseEntity possiblita personalizar as respostas das requisições
         @PostMapping("/criar")
-        public NinjaDTO criarNinja(@RequestBody NinjaDTO ninja){
-
-        return ninjaService.criarNinja(ninja);
+            public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninja){
+            NinjaDTO novoNinja = ninjaService.criarNinja(ninja);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                .body("ninja criado com sucesso: " + novoNinja);
         }
 
         // mostrar todos os ninjas CREATE
 
         @GetMapping("/listar/{Id}")
-        public NinjaDTO listarId(@PathVariable Long id){
+            public ResponseEntity<?> listarId(@PathVariable Long id){
 
-        return ninjaService.BuscarPorId(id);
+                NinjaDTO ninja = ninjaService.BuscarPorId(id);
+
+                if(ninja == null){
+                    return ResponseEntity.ok("ninja encontrado: "+ ninja);
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ninja não encontrado");
+                }
+
+
         }
 
         //listar todos os ninjas
         @GetMapping("/listar")
-        public List<NinjaDTO> ListarNinjas(){
+            public ResponseEntity<List<NinjaDTO>> ListarNinjas(){
+            List<NinjaDTO> ninjas = ninjaService.listarNinjas();
 
-       return ninjaService.listarNinjas();
+            return ResponseEntity.ok(ninjas);
         }
-
-        // mostrar ninja por id
-
-        @GetMapping("/ninjaId")
-        public String MostrarNinjaId(){
-        return "mostrando ninjaid";
-    }
-
 
         //alterar dados UPDATE
 
         @PutMapping("/alterar/{id}")
-        public NinjaDTO AlterarPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado){
-        return ninjaService.Atualizar(id,ninjaAtualizado);
+        public ResponseEntity<?> alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) {
+            NinjaDTO ninja = ninjaService.Atualizar(id, ninjaAtualizado);
+            if (ninja != null) {
+                return ResponseEntity.ok(ninja);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Ninja com o id: " + id + " nao existe nos nossos registros");
+            }
         }
 
 
         // deletar ninja DELETE
         @DeleteMapping("/apagar/{Id}")
-    public void ApagarId(@PathVariable Long Id){
-        ninjaService.deletarNinja(Id);
+            public ResponseEntity<String> ApagarId(@PathVariable Long Id){
+
+            if (ninjaService.BuscarPorId(Id) == null) {
+                ninjaService.deletarNinja(Id);
+                return ResponseEntity.ok("ninja deletado com sucesso");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("o ninja que voce digitou não existe");
+            }
+
         }
-
-
         @DeleteMapping("/deletarTudo")
-        public void deletarTudo(){
+            public void deletarTudo(){
        ninjaService.deletarTudo();
         }
 
